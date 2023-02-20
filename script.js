5534c2016dc808c5612a5331bb570601d83319ce
Adam Ibrahimi <ibradam1@umbc.edu>
2023-02-20 00:16:18 -0500

    Modify grid to have user inputted dimensions

--- script.js
+++ script.js
@@ -1,16 +1,45 @@
-function createGrid() {
+
+let gridSize = 16;
+
+
+function createGrid(gridSize) {
     let div;
     const container = document.querySelector('.grid-container');
-    for (let i = 0; i < 16 * 16; i++){
+    for (let i = 0; i < gridSize * gridSize; i++){
         div = document.createElement('div');
         div.classList.add('cell');
-        div.style.backgroundColor = 'red';
-        div.textContent = 1;
+        container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;   
 
-        
+        div.addEventListener('mouseover', (e) => {
+            e.target.classList.add('colored');
+        });
+
         container.appendChild(div);
-
     }
 }
 
-createGrid();
+
+
+
+const sizeButton = document.querySelector('.size');
+sizeButton.addEventListener('click', () => {
+    gridSize = prompt("Enter grid dimension between 1 - 100: ");
+    while (gridSize > 100 || gridSize < 1) {
+        alert("Invalid dimension.");
+        gridSize = prompt("Enter grid dimension between 1 - 100:");
+    }
+
+    const container = document.querySelector('.grid-container');
+    let divs = document.querySelectorAll('.cell');
+    divs.forEach((div) => {
+        container.removeChild(div);
+    })
+
+    createGrid(gridSize);
+});
+
+createGrid(gridSize);
+
+
+
